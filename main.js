848016321f30ba6cbb6ab73632c7ac486361d90d
RafM <rkjmosiolek@gmail.com>
2017-03-19 20:08:36 +0100

    string.replace() method added, placeholders changed

--- main.js
+++ main.js
@@ -27,6 +27,10 @@ function result(){
     var xItem = randomValueFromArray(insertX);
     var yItem = randomValueFromArray(insertY);
     var zItem = randomValueFromArray(insertZ);
+
+    newStory = newStory.replace(":insertx:", xItem);
+    newStory = newStory.replace(":inserty:", yItem);
+    newStory = newStory.replace(":insertz:", zItem);
     
     if(customName.value != ""){
         var name = customName.value;
